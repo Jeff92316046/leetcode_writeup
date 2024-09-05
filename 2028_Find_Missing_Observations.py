0ec03ac3d1795cac0e84a7f914ad4397433d6221
JEFF Liao <jeffliao92316046@gmail.com>
2024-09-05 22:36:22 +0800

    owo

--- 2028_Find_Missing_Observations.py
+++ 2028_Find_Missing_Observations.py
@@ -2,17 +2,14 @@ import math
 class Solution:
     def missingRolls(self, rolls: list[int], mean: int, n: int) -> list[int]:
         lenght = len(rolls) + n
-        sum_of_rolls = 0
-        for i in rolls:
-            sum_of_rolls += i
+        sum_of_rolls = sum(rolls,0)
         missing_rolls_of_sum = mean*lenght - sum_of_rolls
         if missing_rolls_of_sum>n*6 or missing_rolls_of_sum<0:
             return []
         else :
             if math.floor(missing_rolls_of_sum/n) == 0:
                 return []
-            missing_rolls = [math.floor(missing_rolls_of_sum/n) for _ in range(n)]
+            base_value = math.floor(missing_rolls_of_sum/n)
             temp_rolls_count = math.floor(missing_rolls_of_sum/n)*n
-            for i in range(missing_rolls_of_sum - temp_rolls_count):
-                missing_rolls[i] += 1
-            return missing_rolls
+            left_missing_rolls_of_value = missing_rolls_of_sum - temp_rolls_count
+            return [base_value+1 if i<left_missing_rolls_of_value else base_value for i in range(n)]
